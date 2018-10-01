0ec303781740ec0df27aef0abfaecf78014e68d1
legion <1931330336@qq.com>
2018-10-01 22:21:10 +0800

    bugfix

--- get_capacity/urls.py
+++ get_capacity/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('get_capacity.views',
     (r'^$', 'home'),
     # 表单下拉数据获取及渲染
     (r'^get_app/$', 'get_app'),
-    (r'$get_ip_by_appid/$', 'get_ip_by_appid'),
+    (r'^get_ip_by_appid/$', 'get_ip_by_appid'),
     (r'^get_task_list/$', 'get_task_id_by_appid'),
 
     # 执行作业，获取磁盘容量
