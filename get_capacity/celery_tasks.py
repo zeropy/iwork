a94df058c2e24cac3825d19573efbbea110cbf1f
legion <1931330336@qq.com>
2018-10-04 18:02:23 +0800

    debug

--- get_capacity/celery_tasks.py
+++ get_capacity/celery_tasks.py
@@ -29,6 +29,6 @@ def save_task_log(username='admin'):
     # task_instance_id = execute_job_task(task_instance_id, '192.168.122.100', username)
     task_instance_id = execute_job_task(client, 2, 2)
     while True:
-        is_finish = get_task_capacity(task_instance_id, '192.168.122.100', username)
+        is_finish = get_task_capacity(client, task_instance_id)
         if is_finish:
             break
